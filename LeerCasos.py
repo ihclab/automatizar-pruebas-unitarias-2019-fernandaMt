37f7729c51b4063f299732d88fcf83827240e543
Fernanda Lizeth Morales Torres <fmorales3@ucol.mx>
2019-03-29 16:44:34 -0600

    Problema 03 - Procesado (parsing) del caso de prueba

--- LeerCasos.py
+++ LeerCasos.py
@@ -1,15 +1,26 @@
 def leerCasosPrueba(filename):
     file = open(filename, "r")
+    casos = []
 
     for line in file:
-        print(line)
+        linelist = line.split(':')
+        caso = {
+            'no': linelist[0],
+            'funcion': linelist[1],
+            'entrada': linelist[2],
+            'salida': linelist[3],
+        }
+        casos.append(caso)
 
     file.close()
+    return casos
 
 
 def main():
-    leerCasosPrueba("CasosPrueba.txt")
-
+    casos = leerCasosPrueba("CasosPrueba.txt")
+    for caso in casos:
+        if caso['no'] == '0005':
+            print(caso)
 
 
 if __name__ == "__main__":
