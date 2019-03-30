968270674af46053aab98e68d96f6ad85136d2b1
Fernanda Lizeth Morales Torres <fmorales3@ucol.mx>
2019-03-29 20:19:47 -0600

    Actividad 01 - actividad complementaria, registro de resultados e indicadores de colores

--- LeerCasos.py
+++ LeerCasos.py
@@ -63,12 +63,20 @@ def probarCasos(casos):
 
     return casos
 
+def pintarResultados(resultados):
+    for resultado in resultados:
+        if resultado['valido'] == 'Exito':
+            print('\u001b[32m')
+        else:
+            print('\u001b[31m')
+        pprint.pprint(resultado)
+
 
 def main():
     casos = leerCasosPrueba("CasosPrueba.txt")
     casosConverted = conversionTipos(casos)
     resultados = probarCasos(casosConverted)
-    pprint.pprint(resultados)
+    pintarResultados(resultados)
 
 
 if __name__ == "__main__":
